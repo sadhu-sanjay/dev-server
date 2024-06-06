40bf9ddbfb47b1f1e7d39d975680286c96896ba0
sadhu-sanjay <sanjaygoswami60@gmail.com>
2024-06-06 07:01:54 +0530

    working with new app ts

--- app.ts
+++ app.ts
@@ -1,5 +1,10 @@
+import express from 'express';
+import * as http from 'http'
+import cors from 'cors';
 
-const app: express.Application = express()
 
-const server: 
+const app: express.Application = express();
+app.use(cors());
+
+const server: http.Server = http.createServer(app);
 
--- index.mjs
+++ index.mjs
@@ -1,72 +1,63 @@
+import { createServer } from "node:http";
+import { createReadStream } from "node:fs";
+import { runQuery } from "./db.mjs";
+import express from "express";
 
-import { createServer } from 'node:http';
-import { createReadStream } from 'node:fs';
-import { runQuery } from './db.mjs';
-import express from 'express'
+const port = 9000;
+const host = "127.0.0.1";
 
-const port = 9000
-const host = '127.0.0.1'
-
-const app = express()
+const app = express();
 
 const server = createServer(async (req, res) => {
-    
-    console.log("Request", req.url)
+  console.log("Request", req.url);
 
-     // Set CORS headers
-    res.setHeader('Access-Control-Allow-Origin', '*'); // Allow requests from any origin
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); // Allow the specified methods
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Allow the specified headers
+  // Set CORS headers
+  res.setHeader("Access-Control-Allow-Origin", "*"); // Allow requests from any origin
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE"); // Allow the specified methods
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization"); // Allow the specified headers
 
-    // Check if it's a pre-flight request
-    if (req.method === 'OPTIONS') {
-        // Respond to pre-flight request
-        res.writeHead(200);
-        res.end();
-        return;
-    }
+  // Check if it's a pre-flight request
+  if (req.method === "OPTIONS") {
+    // Respond to pre-flight request
+    res.writeHead(200);
+    res.end();
+    return;
+  }
 
-    if (req.url == "/gpt") {
+  if (req.url == "/gpt") {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    const fileData = createReadStream("data.json", "utf-8");
 
-        res.writeHead(200, { 'Content-Type': 'application/json' });
-        const fileData = createReadStream('data.json', 'utf-8')
+    fileData.on("error", (error) => {
+      console.error("error reading File", error);
+      res.writeHead(500, { "Content-Type": "text/plain" });
+      res.end("Error reading data file");
+    });
 
-        fileData.on('error', (error) => {
-            console.error("error reading File", error)
-            res.writeHead(500, {'Content-Type': 'text/plain'});
-            res.end("Error reading data file")
-        })
+    fileData.on("data", (chunk) => {
+      res.write(chunk);
+    });
 
-        fileData.on('data', (chunk => {
-            res.write(chunk)
-        }))
-
-        fileData.on('end', (chunk => {
-            res.end()
-        }))
-    } else if (req.url == "/sample") {
-      const query = "select * from Records limit 100";
-      runQuery(query)
-        .then((value) => {
-            console.log("Value Here", value);
-            res.writeHead(200, {'Content-Type': 'application/json'})
-            res.end(JSON.stringify(value));
-        })
-        .catch((rsn) => {
-          console.log("reason", rsn);
-          res.writeHead(500, {'Content-Type': 'text/plain'})
-          res.end("Error In Running Query")
-        })
-    }
-
+    fileData.on("end", (chunk) => {
+      res.end();
+    });
+  } else if (req.url == "/sample") {
+    const query = "select * from Records limit 100";
+    runQuery(query)
+      .then((value) => {
+        console.log("Value Here", value);
+        res.writeHead(200, { "Content-Type": "application/json" });
+        res.end(JSON.stringify(value));
+      })
+      .catch((rsn) => {
+        console.log("reason", rsn);
+        res.writeHead(500, { "Content-Type": "text/plain" });
+        res.end("Error In Running Query");
+      });
+  }
 });
 
 // Starts a simple http server locally on port 3000;
 server.listen(port, host, () => {
-    console.log("Port", port, "Host", host)
-})
-
-
-const app: express.Application = express();
-const server: http.Server = http.createServer(app);
-
+  console.log("Port", port, "Host", host);
+});
