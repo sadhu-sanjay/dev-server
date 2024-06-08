79f60eb63ed1c48535330d0665887c6404c7af4c
sadhu-sanjay <sanjaygoswami60@gmail.com>
2024-06-08 21:05:34 +0530

    removed app.,ts

--- index.mjs
+++ index.mjs
@@ -5,59 +5,57 @@ import express from "express";
 
 const port = 9000;
 const host = "127.0.0.1";
+const UsersRouter = require
 
 const app = express();
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
-const server = createServer(async (req, res) => {
-  console.log("Request", req.url);
-
-  // Set CORS headers
-  res.setHeader("Access-Control-Allow-Origin", "*"); // Allow requests from any origin
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE"); // Allow the specified methods
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization"); // Allow the specified headers
-
-  // Check if it's a pre-flight request
   if (req.method === "OPTIONS") {
-    // Respond to pre-flight request
-    res.writeHead(200);
+    res.send(200);
+  }else {
+    next();
+  }
+})
+
+app.get("/gpt", async (req, res) => {
+  const fileData = createReadStream("data.json", "utf-8");
+
+  fileData.on("error", (error) => {
+    console.error("error reading File", error);
+    res.writeHead(500, { "Content-Type": "text/plain" });
+    res.end("Error reading data file");
+  });
+
+  fileData.on("data", (chunk) => {
+    res.write(chunk);
+  });
+
+  fileData.on("end", (chunk) => {
     res.end();
-    return;
-  }
+  });
+})
 
-  if (req.url == "/gpt") {
-    res.writeHead(200, { "Content-Type": "application/json" });
-    const fileData = createReadStream("data.json", "utf-8");
-
-    fileData.on("error", (error) => {
-      console.error("error reading File", error);
+app.get("/sample", async (req, res) => {
+  const query = "select * from Records limit 100";
+  runQuery(query)
+    .then((value) => {
+      console.log("Value Here", value);
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(JSON.stringify(value));
+    })
+    .catch((rsn) => {
+      console.log("reason", rsn);
       res.writeHead(500, { "Content-Type": "text/plain" });
-      res.end("Error reading data file");
+      res.end("Error In Running Query");
     });
+})
 
-    fileData.on("data", (chunk) => {
-      res.write(chunk);
-    });
-
-    fileData.on("end", (chunk) => {
-      res.end();
-    });
-  } else if (req.url == "/sample") {
-    const query = "select * from Records limit 100";
-    runQuery(query)
-      .then((value) => {
-        console.log("Value Here", value);
-        res.writeHead(200, { "Content-Type": "application/json" });
-        res.end(JSON.stringify(value));
-      })
-      .catch((rsn) => {
-        console.log("reason", rsn);
-        res.writeHead(500, { "Content-Type": "text/plain" });
-        res.end("Error In Running Query");
-      });
-  }
-});
 
 // Starts a simple http server locally on port 3000;
+
 server.listen(port, host, () => {
   console.log("Port", port, "Host", host);
-});
+})
